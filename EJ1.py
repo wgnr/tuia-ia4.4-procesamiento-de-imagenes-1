061f72f2142a2a7c8112396b798be53e993bce35
Juan Wagner <juanswagner@gmail.com>
2023-10-03 14:24:22 -0300

    format document

--- EJ1.py
+++ EJ1.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def local_histogram_equalization(image, window_size):
     # Obtener dimensiones de la imagen
     height, width = image.shape
@@ -11,7 +12,14 @@ def local_histogram_equalization(image, window_size):
 
     # Agregar bordes a la imagen
     border_type = cv2.BORDER_REPLICATE
-    image_with_border = cv2.copyMakeBorder(image, half_window_size, half_window_size, half_window_size, half_window_size, border_type)
+    image_with_border = cv2.copyMakeBorder(
+        image,
+        half_window_size,
+        half_window_size,
+        half_window_size,
+        half_window_size,
+        border_type,
+    )
 
     # Crear una copia de la imagen para almacenar el resultado
     result_image = np.copy(image)
@@ -31,12 +39,15 @@ def local_histogram_equalization(image, window_size):
             window_equalized = cv2.equalizeHist(window)
 
             # Asignar la ventana ecualizada al resultado
-            result_image[y - half_window_size, x - half_window_size] = window_equalized[half_window_size, half_window_size]
+            result_image[y - half_window_size, x - half_window_size] = window_equalized[
+                half_window_size, half_window_size
+            ]
 
     return result_image
 
+
 # Cargar la imagen
-image = cv2.imread('img/Imagen_con_detalles_escondidos.tif', cv2.IMREAD_GRAYSCALE)
+image = cv2.imread("img/Imagen_con_detalles_escondidos.tif", cv2.IMREAD_GRAYSCALE)
 
 # Tamaño de la ventana de procesamiento (ajustar según sea necesario)
 window_size = 21
@@ -44,17 +55,17 @@ window_size = 21
 # Aplicar la ecualización local del histograma con umbral
 equalized_image = local_histogram_equalization(image, window_size)
 
-#Eliminar ruido 
+# Eliminar ruido
 result_image = cv2.medianBlur(equalized_image, 3)
 
 # Mostrar la imagen original y la imagen procesada
-fig, axs = plt.subplots(1,3, figsize=(10,4), sharex=True, sharey=True)
+fig, axs = plt.subplots(1, 3, figsize=(10, 4), sharex=True, sharey=True)
 fig.suptitle("Ecualización local de histograma")
-axs[0].imshow(image, cmap='gray')
+axs[0].imshow(image, cmap="gray")
 axs[0].set_title("Imagen Original")
-axs[1].imshow(equalized_image, cmap='gray')
+axs[1].imshow(equalized_image, cmap="gray")
 axs[1].set_title("Imagen Ecualizada")
-axs[2].imshow(result_image, cmap='gray')
+axs[2].imshow(result_image, cmap="gray")
 axs[2].set_title("Imagen Suavizada")
 fig.tight_layout()
 fig.show()
